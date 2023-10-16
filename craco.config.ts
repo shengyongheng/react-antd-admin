00b44324f525c5e1b2846acc2cc634de7e99fe2f
hsy <3271660881@qq.com>
2023-10-16 22:03:01 +0800

    feat:依赖报错修复

--- craco.config.ts
+++ craco.config.ts
@@ -1,21 +1,22 @@
-export const path = require('path');
-const resolve = (dir: string) => path.resolve(__dirname, dir);
+export const path = require('path')
+const resolve = (dir: string) => path.resolve(__dirname, dir)
 const cracoSassResourcesLoader = require('craco-sass-resources-loader')
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@components': resolve('src/components'),
-            '@hooks': resolve('src/hooks'),
-            '@utils': resolve('src/utils')
-        },
-    },
-    plugins: [
-        {
-            plugin: cracoSassResourcesLoader,
-            options: {
-                resources: path.resolve(__dirname, 'src/styles/globalVars.scss')
-            }
-        }
-    ]
-};
+  webpack: {
+    alias: {
+      '@components': resolve('src/components'),
+      '@hooks': resolve('src/hooks'),
+      '@utils': resolve('src/utils'),
+      '@styles': resolve('src/styles')
+    }
+  },
+  plugins: [
+    {
+      plugin: cracoSassResourcesLoader,
+      options: {
+        resources: path.resolve(__dirname, 'src/styles/globalVars.scss')
+      }
+    }
+  ]
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,18 @@
-import React, { FC, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { } from 'antd'
-import styles from "./index.module.scss"
-interface IProps {
-
-}
+import React, { FC, useState, useEffect } from 'react'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import {} from 'antd'
+import styles from './index.module.scss'
+interface IProps {}
 const HomePage: FC = (props: IProps): React.JSX.Element => {
-    const token = useSelector((state: any) => state.user.token)
-    console.log(token, 'user.token');
-    useEffect(() => {
-
-    }, []);
-    return (
-        <>
-            token:{token}
-            <div className={styles['test']}>Home</div>
-        </>
-    )
+  const token = useSelector((state: any) => state.user.token)
+  console.log(token, 'user.token')
+  useEffect(() => {}, [])
+  return (
+    <>
+      token:{token}
+      <div className={styles['test']}>Home</div>
+    </>
+  )
 }
 
 export default HomePage
